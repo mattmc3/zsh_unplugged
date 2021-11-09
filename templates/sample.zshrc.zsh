658a78ea5d320c43ab74b460ba95ad82ddbdf486
mattmc3 <mattmc3@gmail.com>
2021-11-09 12:29:03 -0500

    Style changes

--- templates/sample.zshrc.zsh
+++ templates/sample.zshrc.zsh
@@ -2,29 +2,28 @@ ZPLUGINDIR=$HOME/.zsh/plugins
 
 # if you want to use unplugged, you can copy/paste plugin-clone here, or just pull the repo
 if [[ ! -d $ZPLUGINDIR/zsh_unplugged ]]; then
-    git clone https://github.com/mattmc3/zsh_unplugged $ZPLUGINDIR/zsh_unplugged
+  git clone https://github.com/mattmc3/zsh_unplugged $ZPLUGINDIR/zsh_unplugged
 fi
 source $ZPLUGINDIR/zsh_unplugged/unplugged.zsh
 
 plugins=(
-    # core plugins
-    mafredri/zsh-async
-    zsh-users/zsh-autosuggestions
-    zsh-users/zsh-history-substring-search
+  # core plugins
+  mafredri/zsh-async
+  zsh-users/zsh-autosuggestions
+  zsh-users/zsh-history-substring-search
 
-    # user plugins
-    rupa/z
-    peterhurford/up.zsh
-    rummik/zsh-tailf
+  # user plugins
+  rupa/z
+  peterhurford/up.zsh
+  rummik/zsh-tailf
 
-    # prompts
-    sindresorhus/pure
+  # prompts
+  sindresorhus/pure
 
-    # load this one last
-    zsh-users/zsh-syntax-highlighting
+  # load this one last
+  zsh-users/zsh-syntax-highlighting
 )
-for plugin in $plugins
-do
+for plugin in $plugins; do
     plugin-clone https://github.com/${plugin}.git
     source $ZPLUGINDIR/${plugin:t}/init.zsh
 done
