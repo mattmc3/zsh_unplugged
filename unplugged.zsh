802823cbe920676e7514a6b88ddb68655b98e420
mattmc3 <mattmc3@gmail.com>
2021-12-30 09:27:32 -0500

    Refactor to ~20 lines and fix #3

--- unplugged.zsh
+++ unplugged.zsh
@@ -1,37 +1,35 @@
 # zunplugged: https://github.com/mattmc3/zsh_unplugged
 # a simple, ultra-fast plugin handler
 
+# clone a plugin, identify its init file, source it (with zsh-defer if available)
+# and add it to your fpath
 function plugin-load() {
-  # clone a plugin, identify the plugin's init file, source it (with defer if possible),
-  # and add it to your fpath
   local repo plugin_name plugin_dir initfile initfiles
   ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-~/.config/zsh}/plugins}
   for repo in $@; do
     plugin_name=${repo:t}
     plugin_dir=$ZPLUGINDIR/$plugin_name
     initfile=$plugin_dir/$plugin_name.plugin.zsh
-    [[ -d $plugin_dir ]] \
-      || git clone --depth 1 --recursive --shallow-submodules https://github.com/$repo $plugin_dir
+    if [[ ! -d $plugin_dir ]]; then
+      echo "Cloning $repo"
+      git clone -q --depth 1 --recursive --shallow-submodules https://github.com/$repo $plugin_dir
+    fi
     if [[ ! -e $initfile ]]; then
       initfiles=($plugin_dir/*.plugin.{z,}sh(N) $plugin_dir/*.{z,}sh(N))
       [[ ${#initfiles[@]} -gt 0 ]] || { echo >&2 "Plugin has no init file '$repo'." && continue }
       ln -s "${initfiles[1]}" "$initfile"
     fi
     fpath+=$plugin_dir
-    if (( $+functions[zsh-defer] )); then
-      zsh-defer source $initfile
-    else
-      source $initfile
-    fi
+    (( $+functions[zsh-defer] )) && zsh-defer . $initfile || . $initfile
   done
 }
 
 # if you want to compile your plugins you may see performance gains
 function plugin-compile () {
-  local plugindir="${ZPLUGINDIR:-$HOME/.zsh/plugins}"
+  ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-~/.config/zsh}/plugins}
   autoload -U zrecompile
   local f
-  for f in $plugindir/**/*.zsh{,-theme}(N); do
+  for f in $ZPLUGINDIR/**/*.zsh{,-theme}(N); do
     zrecompile -pq "$f"
   done
 }
