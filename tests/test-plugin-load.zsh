dd461f50e84b3ba506770c685dde79c4f4056ebc
mattmc3 <mattmc3@gmail.com>
2021-12-31 10:33:08 -0500

    Add more tests

--- tests/test-plugin-load.zsh
+++ tests/test-plugin-load.zsh
@@ -7,6 +7,18 @@ source $PROJECT_HOME/zunplugged.zsh
 @test "zunplugged sourced successfully" $? -eq 0
 @test "plugin-load function exists" "$+functions[plugin-load]" -eq 1
 
+@test "zsh-defer function does not exist" "$+functions[zsh-defer]" -eq 0
+plugin-load romkatv/zsh-defer &>/dev/null
+@test "plugin-load succeeded with exit code 0" $? -eq 0
+@test "zsh-defer function exists" "$+functions[zsh-defer]" -eq 1
+
+zwcfiles=($ZPLUGINDIR/**/*.zwc(N))
+@test "no .zwc files exist" "${#zwcfiles[@]}" -eq 0
+plugin-compile &>/dev/null
+@test "plugin-compile succeeded with exit code 0" $? -eq 0
+zwcfiles=($ZPLUGINDIR/**/*.zwc(N))
+@test ".zwc files were created" "${#zwcfiles[@]}" -gt 0
+
 if [[ -d $ZPLUGINDIR ]] && [[ $ZPLUGINDIR = *ztap* ]]; then
   rm -rf $ZPLUGINDIR
 fi
