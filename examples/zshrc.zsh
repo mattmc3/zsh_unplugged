94945ddf31fd58391363cb0d9f8b2de685256e27
mattmc3 <mattmc3@gmail.com>
2022-09-13 11:14:47 -0400

    Update zshrc example

--- examples/zshrc.zsh
+++ examples/zshrc.zsh
@@ -2,55 +2,39 @@
 #ZPLUGINDIR=$ZDOTDIR/plugins
 
 # declare a simple plugin-load function
-function plugin-load() {
-  local repo plugin_name plugin_dir initfile initfiles
+function plugin-load {
+  local repo plugdir initfile
   ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
   for repo in $@; do
-    plugin_name=${repo:t}
-    plugin_dir=$ZPLUGINDIR/$plugin_name
-    initfile=$plugin_dir/$plugin_name.plugin.zsh
-    if [[ ! -d $plugin_dir ]]; then
-      echo "Cloning $repo"
-      git clone -q --depth 1 --recursive --shallow-submodules https://github.com/$repo $plugin_dir
+    plugdir=$ZPLUGINDIR/${repo:t}
+    initfile=$plugdir/${repo:t}.plugin.zsh
+    if [[ ! -d $plugdir ]]; then
+      echo "Cloning $repo..."
+      git clone -q --depth 1 --recursive --shallow-submodules https://github.com/$repo $plugdir
     fi
     if [[ ! -e $initfile ]]; then
-      initfiles=($plugin_dir/*.plugin.{z,}sh(N) $plugin_dir/*.{z,}sh{-theme,}(N))
-      [[ ${#initfiles[@]} -gt 0 ]] || { echo >&2 "Plugin has no init file '$repo'." && continue }
-      ln -s "${initfiles[1]}" "$initfile"
+      local -a initfiles=($plugdir/*.plugin.{z,}sh(N) $plugdir/*.{z,}sh{-theme,}(N))
+      (( $#initfiles )) || { echo >&2 "No init file found '$repo'." && continue }
+      ln -sf "${initfiles[1]}" "$initfile"
     fi
-    fpath+=$plugin_dir
+    fpath+=$plugdir
     (( $+functions[zsh-defer] )) && zsh-defer . $initfile || . $initfile
   done
 }
 
 # make a github repo plugins list
 plugins=(
-  # load these first
-  zshzoo/zshrc.d
   sindresorhus/pure
-  romkatv/zsh-defer
-
-  # other plugins, all at hypersonic load speeds with zsh-defer
-  zshzoo/setopts
-  zshzoo/history
-  zshzoo/keybindings
-  zshzoo/zstyle-completions
-  zsh-users/zsh-autosuggestions
-  zsh-users/zsh-history-substring-search
   mattmc3/zman
-  olets/zsh-abbr
-  zshzoo/copier
-  zshzoo/macos
-  zshzoo/prj
-  zshzoo/magic-enter
-  zshzoo/zfishcmds
-  zshzoo/termtitle
   rupa/z
   rummik/zsh-tailf
   peterhurford/up.zsh
+  zsh-users/zsh-autosuggestions
+  zsh-users/zsh-history-substring-search
 
-  # load these last
-  zshzoo/compinit
+  # load these at hypersonic load speeds with zsh-defer
+  romkatv/zsh-defer
+  olets/zsh-abbr
   zdharma-continuum/fast-syntax-highlighting
 )
 plugin-load $plugins
