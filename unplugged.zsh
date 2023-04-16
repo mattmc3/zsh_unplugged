0cf02c81688a5fb441e25ae1601a57531806fdc3
mattmc3 <mattmc3@gmail.com>
2023-04-16 14:04:15 -0400

    Change unplugged.zsh to contain plugin-load

---
+++ unplugged.zsh
@@ -0,0 +1,35 @@
+#
+# unplugged.zsh - https://github.com/mattmc3/zsh_unplugged
+#
+# A simple, ultra-fast, minimalist plugin management function in <20 lines of code.
+#
+# Usage:
+# source ${ZDOTDIR:-~}/unplugged.zsh
+# repos=(
+#   zsh-users/zsh-syntax-highlighting
+#   zsh-users/zsh-autosuggestions
+#   zsh-users/zsh-history-substring-search
+# )
+# plugin-load $repos
+#
+
+##? Clone a plugin, identify its init file, source it, and add it to your fpath.
+function plugin-load {
+  local repo plugdir initfile
+  ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
+  for repo in $@; do
+    plugdir=$ZPLUGINDIR/${repo:t}
+    initfile=$plugdir/${repo:t}.plugin.zsh
+    if [[ ! -d $plugdir ]]; then
+      echo "Cloning $repo..."
+      git clone -q --depth 1 --recursive --shallow-submodules https://github.com/$repo $plugdir
+    fi
+    if [[ ! -e $initfile ]]; then
+      local -a initfiles=($plugdir/*.plugin.{z,}sh(N) $plugdir/*.{z,}sh{-theme,}(N))
+      (( $#initfiles )) || { echo >&2 "No init file found '$repo'." && continue }
+      ln -sf "${initfiles[1]}" "$initfile"
+    fi
+    fpath+=$plugdir
+    (( $+functions[zsh-defer] )) && zsh-defer . $initfile || . $initfile
+  done
+}
